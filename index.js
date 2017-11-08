32d3ba296bc9d23c03d5bfeaeb5030ec2b3a52d8
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-08 17:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ function scuberGreetingForFeet(distance) {
      }
    }
  }
+
+ function ternaryCheckCity(city) {
+   let message;
+   console.log(city)
+   city === 'NYC' ? message = 'Ok, sounds good.' : message = 'No go.';
+   console.log(message)
+   return message
+ }
  
- +function ternaryCheckCity(city) {
- +  let message;
- +  console.log(city)
- +  city === 'NYC' ? message = 'Ok, sounds good.' : message = 'No go.';
- +  console.log(message)
- +  return message
- +}
- +
- +function switchOnCharmFromTip(tip) {
- +  switch (tip) {
- +  case 'generous':
- +    return('Thank you so much.')
- +    break;
- +  case 'not as generous':
- +    return('Thank you.')
- +    break;
- +  case 'thanks for everything':
- +    return('Bye.')
+ function switchOnCharmFromTip(tip) {
+  switch (tip) {
+   case 'generous':
+     return('Thank you so much.')
+     break;
+   case 'not as generous':
+     return('Thank you.')
+     break;
+   case 'thanks for everything':
+     return('Bye.')
