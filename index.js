a4c5a798097193f68c7dbd59e2dffc62e079e6e2
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-08 17:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-// Write your code in this file!
-//const totalFeet = 199;
-//const city = 'NYC';
-//const tip = 'generous';
-
-function scuberGreetingForFeet(totalFeet){
-   if(totalFeet <= 400){
-     return 'This one is on me!';
-   } else if (totalFeet > 2001 && totalFeet <= 2501){
+function scuberGreetingForFeet(ride){
+   if(ride <= 400){
+     return "This one is on me!";
+   } else if (ride > 2000 && ride <= 2500){
        return "I will gladly take your thirty bucks.";
-   } else {
-     return "No can do.";
+   } else if (ride > 2500) {
+      return "No can do.";
    }
  }
 
  function ternaryCheckCity(city){
-   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+   return city === 'NYC' ? "Ok, sounds good." : "No go."
  }
 
  function switchOnCharmFromTip(tip){
    switch (tip) {
      case 'generous':
-       return 'Thank you so much.'
+       return "Thank you so much."
        break;
+       
      case 'not as generous':
-       return 'Thank you.'
+       return "Thank you."
      default:
-       return 'Bye.'
+       return "Bye."
    }
